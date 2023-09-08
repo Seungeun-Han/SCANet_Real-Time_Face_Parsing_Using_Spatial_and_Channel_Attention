bd06b7e5a446b706aa4c37df1996596b5add309a
Seungeun Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-09-08 13:45:35 +0900

    Update visualization.py

--- Face_Parsing_Demo/visualization.py
+++ Face_Parsing_Demo/visualization.py
@@ -2,33 +2,17 @@ import os
 import cv2
 import numpy as np
 
-"""LABELS = {'bg': 0, 'skin': 1, 'nose': 2, 'eye_g': 3, 'l_eye': 4, 'r_eye': 5,
-        'l_brow': 6, 'r_brow': 7, 'l_ear': 8, 'r_ear': 9, 'mouth': 10, 'u_lip': 11,
-        'l_lip': 12, 'hair': 13, 'hat': 14, 'ear_r': 15, 'neck_l': 16, 'neck': 17, 'cloth': 18, 'mask': 19}"""
-LABELS = {'bg': 0, 'skin': 1, 'nose': 2, 'eye_g': 3, 'l_eye': 4, 'r_eye': 5,
-        'l_brow': 6, 'r_brow': 7, 'l_ear': 8, 'r_ear': 9, 'mouth': 10, 'u_lip': 11,
-        'l_lip': 12, 'hair': 13, 'hat': 14, 'ear_r': 15, 'neck': 16, 'cloth': 17, 'neck_l': 18, 'mask': 19, 'hand': 20, 'scarf': 21}
-
 #def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_results/parsing_map_on_im.jpg'):
 def visualization(im, parsing_anno, stride=1, save_im=False, save_path = None, num_of_class = 20):
     # Colors for all 20 parts
-    """part_colors = [[0, 0, 0, 150], [255, 85, 0, 150], [255, 170, 0, 150],  # background, skin, nose
-                   [255, 0, 85, 150], [255, 0, 170, 150],  # glasses, left_eye
-                   [0, 255, 0, 150], [204, 255, 0, 150], [0, 102, 153, 150],  # right_eye, left_brow, r_brow
-                   [0, 255, 85, 150], [255, 190, 190, 150], # left_ear, right_ear
-                   [0, 0, 255, 150], [85, 10, 255, 150], [255, 0, 255, 150],  # mouth, upper_lip, lower_lip
-                   [0, 85, 255, 150], [0, 170, 255, 150], # hair, hat
-                   [255, 255, 0, 150], [255, 255, 85, 150], [255, 255, 170, 150], # ear_ring, necklace, neck
-                   [255, 0, 255, 150],[153, 255, 0, 150]] #23개=>19개로 수정    # cloth, mask """
-
-    part_colors = [[0, 0, 0, 150], [255, 0, 0, 150], [255, 170, 0, 150],  # background, skin, nose
-                   [0, 0, 255, 150], [255, 255, 0, 150], [0, 255, 255, 150],  # glasses, left_eye, right_eye
-                   [255, 0, 255, 150], [255, 255, 170, 150],  # left_brow, r_brow
-                   [255, 170, 255, 150], [170, 255, 255, 150],  # left_ear, right_ear
-                   [170, 170, 255, 150], [170, 255, 170, 150], [255, 170, 170, 150],  # mouth, upper_lip, lower_lip
-                   [0, 85, 255, 150], [0, 102, 153, 150],  # hair, hat
-                   [85, 255, 0, 150], [85, 0, 255, 150], [85, 255, 255, 150],  # ear_ring, necklace, neck
-                   [255, 0, 85, 150], [153, 255, 0, 150], [255, 190, 190, 150], [204, 255, 0, 150]]  # cloth, mask
+    part_colors = [[0, 0, 0, 150], [255, 0, 0, 150], [255, 170, 0, 150],  
+                   [0, 0, 255, 150], [255, 255, 0, 150], [0, 255, 255, 150],  
+                   [255, 0, 255, 150], [255, 255, 170, 150], 
+                   [255, 170, 255, 150], [170, 255, 255, 150],
+                   [170, 170, 255, 150], [170, 255, 170, 150], [255, 170, 170, 150], 
+                   [0, 85, 255, 150], [0, 102, 153, 150], 
+                   [85, 255, 0, 150], [85, 0, 255, 150], [85, 255, 255, 150], 
+                   [255, 0, 85, 150], [153, 255, 0, 150], [255, 190, 190, 150], [204, 255, 0, 150]]  
 
 
     im = np.array(im)
